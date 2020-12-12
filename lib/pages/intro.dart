5273de800ac0ea7ab5d302da6c01445acb8ba564
Bart Dority <b0rgBart3@gmail.com>
2020-12-11 17:01:34 -0800

    programmatic hex codes for tile blur

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,5 +1,7 @@
 // import Google's Material Design library
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'dart:developer' as developer;
 
 class Intro extends StatelessWidget {
   @override
@@ -18,7 +20,7 @@ class Intro extends StatelessWidget {
       "LIGHTBURST",
       textDirection: TextDirection.ltr,
       style: TextStyle(
-        color: Colors.white,
+        color: HexColor("#f2f2f2"),
         decoration: TextDecoration.none,
         fontFamily: 'Heebo',
       ),
@@ -28,31 +30,120 @@ class Intro extends StatelessWidget {
   Widget cross() {
     return Column(
       children: [
-        box(),
+        tile(),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [box(), box(), box()],
+          children: [
+            tile(),
+            playBox(),
+            tile()
+          ],
         ),
-        box()
+        tile()
       ],
     );
   }
 
-  Widget box() {
-    return Padding( 
-      padding: EdgeInsets.all(14.0),
-      child: Container(
+Widget playBox() {
+  return 
+    Stack(
+      alignment: AlignmentDirectional.center,
+    children: [
+    tile(), playText(),
+    ]
+
+
+  );
+  
+}
+
+  Widget playText() {
+    return Text(
+      "Play",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color:Colors.white,
+        decoration: TextDecoration.none,
+        fontFamily: 'Heebo',
+        fontSize: 30,
+        shadows: [
+              Shadow(
+                color: Colors.blue.shade900.withOpacity(1),
+                offset: Offset(0, 0),
+                blurRadius: 15,
+              ),
+            ],
+      )
+    );
+  }
+  Widget tile() {
+    return Padding(
+        padding: EdgeInsets.all(14.0),
+        child: Stack(children:[box(70), box(40)] )
+        );
+  }
+
+  Widget box(size) {
+    return Container(
           alignment: Alignment.center,
           width: 70,
           height: 70,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: Colors.lightBlue,
+            color: HexColor("#34b7e8"),
             boxShadow: [
-              BoxShadow(color: Colors.blue, spreadRadius: 11),
+              BoxShadow(color: darkerHex("#336cb3"), 
+              blurRadius: 10,
+              spreadRadius: 10),
             ],
           ),
-    )
-    );
+        );
+  }
+
+  Color darkerHex(value) {
+
+    developer.log(value, name: 'original hexString');
+    var newString = value;
+    developer.log(newString, name: 'newString');
+    var nString = value.split('');
+   // developer.log(nString.join(''), name: 'nString');
+    var red = nString[1] + nString[2];
+    var green = nString[3] + nString[4];
+    var blue = nString[5] + nString[6];
+
+     developer.log(red, name: 'red');
+
+    var redInt = (int.parse(red, radix: 16) ).toInt();
+    var greenInt = (int.parse(green, radix: 16) ).toInt();
+    var blueInt = (int.parse(blue, radix: 16) ).toInt();
+   
+    developer.log(redInt.toString(), name: 'redInt');
+
+    redInt = (redInt * .8).toInt();
+    greenInt = (greenInt * .8).toInt();
+    blueInt = (blueInt * .8).toInt();
+
+    developer.log(redInt.toString(), name: 'redInt');
+    if (redInt < 0) {
+      redInt = 0;
+    }
+
+   if (greenInt < 0) {
+      greenInt = 0;
+    }
+
+    if (blueInt < 0) {
+      blueInt = 0;
+    }
+
+    var redHex = redInt.toRadixString(16);
+    var greenHex = greenInt.toRadixString(16);
+    var blueHex = blueInt.toRadixString(16);
+
+    developer.log(redHex.toString(), name: 'redHex');
+    var hexString = "#" + redHex + greenHex + blueHex;
+  
+    developer.log(hexString, name: 'hexString');
+    return HexColor(hexString);
   }
 }
