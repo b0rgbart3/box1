b99e9ab06858ca5c69a6ec9f11a118067068002d
Bart Dority <b0rgBart3@gmail.com>
2020-12-17 18:06:18 -0800

    stateless initial animation

---
+++ lib/util/hexfunctions.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+  // darkerHex -- takes in a String that is a hexadecimal color value as a string
+  // takes the RGB values as double char hex values - converts that to an inteber
+  // then darkens it by .2% and then converts it back to a 2 char hex value
+  // and then returns the new Color - based on the new hexadecimal string
+
+  Color darkerHex(value) {
+    //developer.log(value, name: 'original hexString');
+    var newString = value;
+    //developer.log(newString, name: 'newString');
+    var nString = value.split('');
+    // developer.log(nString.join(''), name: 'nString');
+    var red = nString[1] + nString[2];
+    var green = nString[3] + nString[4];
+    var blue = nString[5] + nString[6];
+
+    //developer.log(red, name: 'red');
+
+    var redInt = (int.parse(red, radix: 16)).toInt();
+    var greenInt = (int.parse(green, radix: 16)).toInt();
+    var blueInt = (int.parse(blue, radix: 16)).toInt();
+
+    //developer.log(redInt.toString(), name: 'redInt');
+
+    redInt = (redInt * .8).toInt();
+    greenInt = (greenInt * .8).toInt();
+    blueInt = (blueInt * .8).toInt();
+
+    //developer.log(redInt.toString(), name: 'redInt');
+    if (redInt < 0) {
+      redInt = 0;
+    }
+
+    if (greenInt < 0) {
+      greenInt = 0;
+    }
+
+    if (blueInt < 0) {
+      blueInt = 0;
+    }
+
+    var redHex = redInt.toRadixString(16);
+    var greenHex = greenInt.toRadixString(16);
+    var blueHex = blueInt.toRadixString(16);
+
+    //developer.log(redHex.toString(), name: 'redHex');
+    var hexString = "#" + redHex + greenHex + blueHex;
+
+    //developer.log(hexString, name: 'hexString');
+    return HexColor(hexString);
+  }
+
